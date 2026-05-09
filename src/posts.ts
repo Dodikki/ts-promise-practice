3218b37ba24aee30af406d2a5cc94e3d1bacc141
DORLOK <DORLOK@DESKTOP-J428QQ6>
2026-05-09 23:02:38 +0500

    second commit

--- src/posts.ts
+++ src/posts.ts
@@ -5,19 +5,40 @@ const posts = await loadPosts();
 
 export function renderPosts() {
   posts.forEach((post: Post) => {
-    const li = document.createElement('li')
-    li.innerHTML = `
-    <div class = "onpost">
-      <div class ="posts">
-        <div class = "title">${post.title}</div>
-        <div class = "description">${post.body}</div>
-        <div class = "onbutton">
-        <button class ="link" onclick="document.location='${post.link}'">Link on button</button>
-        </div>
-      </div>
-      <div  class = "img"></div>
-    </div>
-    `
+    const li = document.createElement("li")
+
+    const onpost = document.createElement("div")
+    onpost.classList.add("onpost")
+
+    const posts = document.createElement("div")
+    posts.classList.add("posts")
+
+    const title = document.createElement("div")
+    title.classList.add("title")
+    title.textContent = post.title
+
+    const description = document.createElement("div")
+    description.classList.add("description")
+    description.textContent = post.body
+
+    const onbutton = document.createElement("div")
+    onbutton.classList.add("onbutton")
+
+    const button = document.createElement("button")
+    button.classList.add("link")
+    button.textContent = "Link on button"
+    button.addEventListener("click", () => {
+      document.location.href = post.link
+    })
+
+    const img = document.createElement("div")
+    img.classList.add("img")
+
+    onbutton.appendChild(button)
+    posts.append(title, description, onbutton)
+    onpost.append(posts, img)
+    li.appendChild(onpost)
+
     document.getElementById("list")?.appendChild(li)
   })
 }
